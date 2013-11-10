7ee0e403e4e1753af4d311dcdc06d4aad71ebfb8
Taylor Smears <tsmears17@gmail.com>
2013-11-10 21:03:26 +0100

    edited erb._example.rb

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,12 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+t = 32
+template = ERB.new "t is <%= t + 32 %> minus 32"
+puts template.result(binding)
+
+
+name = "Taylor"
+template = ERB.new "My name is <%= name %>"
+puts template.result(binding)
